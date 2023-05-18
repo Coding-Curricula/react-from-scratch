59fb7dfe85e57526645ac11bb87e26d3da36ca38
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-05-17 19:50:01 -0500

    react counter done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
-    return <div>Hello NY Cohort! My name is Chris.</div>;
+    const [count, setCount] = useState(0)
+
+    function increment() {
+        setCount(count + 1)
+    }
+
+    function decrement() {
+        setCount(count - 1)
+    }
+
+    function reset() {
+        setCount(0)
+    }
+
+    return (
+        <div>
+            <h1>Counter App</h1>
+            <h2>{count}</h2>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
